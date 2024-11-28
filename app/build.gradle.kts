1fc4f1ff7a36ac7cadb1c91a9000bc1bdcb30a21
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-11-28 09:47:34 +0500

    ad libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.muhsantech.statussaver"
         minSdk = 24
+        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -33,6 +34,10 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +50,16 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //
+    implementation ("com.jsibbold:zoomage:1.3.1")
+    implementation ("androidx.media3:media3-exoplayer:1.1.1")
+    implementation ("androidx.media3:media3-exoplayer-dash:1.1.1")
+    implementation ("androidx.media3:media3-ui:1.1.1")
+
+    implementation ("androidx.documentfile:documentfile:1.0.1")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
+    implementation ("com.anggrayudi:storage:1.5.5")
+
 }
