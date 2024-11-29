b94145860e07bb65192f98b23e9de381dbebd34e
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-11-29 08:27:05 +0500

    UI part completed

--- app/src/main/java/com/muhsantech/statussaver/view/activities/MainActivity.kt
+++ app/src/main/java/com/muhsantech/statussaver/view/activities/MainActivity.kt
@@ -2,7 +2,12 @@ package com.muhsantech.statussaver.view.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.devatrii.statussaver.utils.Constants
+import com.muhsantech.statussaver.R
 import com.muhsantech.statussaver.databinding.ActivityMainBinding
+import com.muhsantech.statussaver.utils.replaceFragment
+import com.muhsantech.statussaver.view.fragments.FragmentSettings
+import com.muhsantech.statussaver.view.fragments.FragmentStatus
 
 class MainActivity : AppCompatActivity() {
     private val activity = this
@@ -17,6 +22,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.apply {
+
+            bottomNavigationView.setOnItemSelectedListener {
+                when(it.itemId){
+                    R.id.menu_status->{
+                        //whatspp status
+                        val fragmentWhatsAppStatus = FragmentStatus()
+                        val bundle = Bundle()
+                        bundle.putString(Constants.FRAGMENT_TYPE_KEY, Constants.TYPE_WHATSAPP_MAIN)
+                        replaceFragment(fragmentWhatsAppStatus, bundle)
+                    }
+                    R.id.menu_business_status->{
+                        //whatspp Buusines status
+                        val fragmentWhatsAppStatus = FragmentStatus()
+                        val bundle = Bundle()
+                        bundle.putString(Constants.FRAGMENT_TYPE_KEY, Constants.TYPE_WHATSAPP_BUSINESS)
+                        replaceFragment(fragmentWhatsAppStatus, bundle)
+                    }
+                    R.id.menu_settings->{
+                        replaceFragment(FragmentSettings())
+                    }
+                }
+                return@setOnItemSelectedListener true
+            }
 
         }
 
