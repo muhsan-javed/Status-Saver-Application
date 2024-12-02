42941ffec80ecc9b53325f07d2bd0444274004ca
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-12-02 17:44:38 +0500

    set view Model provider. show data in UI

--- app/src/main/java/com/muhsantech/statussaver/view/fragments/FragmentMedia.kt
+++ app/src/main/java/com/muhsantech/statussaver/view/fragments/FragmentMedia.kt
@@ -5,21 +5,123 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.muhsantech.statussaver.R
+import com.muhsantech.statussaver.data.StatusRepo
 import com.muhsantech.statussaver.databinding.FragmentMediaBinding
+import com.muhsantech.statussaver.models.MediaModel
 import com.muhsantech.statussaver.utils.Constants
+import com.muhsantech.statussaver.view.adapters.MediaAdapter
+import com.muhsantech.statussaver.viewmodels.StatusViewModel
+import com.muhsantech.statussaver.viewmodels.factories.StatusViewModelFactory
 
 class FragmentMedia : Fragment() {
     private val binding by lazy {
         FragmentMediaBinding.inflate(layoutInflater)
     }
 
+    lateinit var viewModel: StatusViewModel
+    lateinit var adapter: MediaAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.apply {
             arguments?.let {
+
+                val repo = StatusRepo(requireActivity())
+                viewModel = ViewModelProvider(
+                    requireActivity(),
+                    StatusViewModelFactory(repo)
+                )[StatusViewModel::class.java]
+
+
                 val mediaType = it.getString(Constants.MEDIA_TYPE_KEY,"")
-                tempMedaiText.text = mediaType
+
+                when (mediaType) {
+                    Constants.MEDIA_TYPE_WHATSAPP_IMAGES -> {
+                        viewModel.whatsAppImagesLiveData.observe(requireActivity()) { unFilteredList ->
+                            val filteredList = unFilteredList.distinctBy { model ->
+                                model.fileName
+                            }
+
+                            val list = ArrayList<MediaModel>()
+                            filteredList.forEach { model ->
+                                list.add(model)
+                            }
+                            adapter = MediaAdapter(list, requireActivity())
+                            mediaRecyclerView.adapter = adapter
+                           /* if (list.size == 0) {
+                                tempMediaText.visibility = View.VISIBLE
+                            } else {
+                                tempMediaText.visibility = View.GONE
+                            }*/
+
+                        }
+                    }
+
+                    Constants.MEDIA_TYPE_WHATSAPP_VIDEOS -> {
+                        viewModel.whatsAppVideosLiveData.observe(requireActivity()) { unFilteredList ->
+                            val filteredList = unFilteredList.distinctBy { model ->
+                                model.fileName
+                            }
+
+                            val list = ArrayList<MediaModel>()
+                            filteredList.forEach { model ->
+                                list.add(model)
+                            }
+                            adapter = MediaAdapter(list, requireActivity())
+                            mediaRecyclerView.adapter = adapter
+                           /* if (list.size == 0) {
+                                tempMediaText.visibility = View.VISIBLE
+                            } else {
+                                tempMediaText.visibility = View.GONE
+                            }*/
+                        }
+                    }
+
+                    Constants.MEDIA_TYPE_WHATSAPP_BUSINESS_IMAGES -> {
+                        viewModel.whatsAppBusinessImagesLiveData.observe(requireActivity()) { unFilteredList ->
+                            val filteredList = unFilteredList.distinctBy { model ->
+                                model.fileName
+                            }
+
+                            val list = ArrayList<MediaModel>()
+                            filteredList.forEach { model ->
+                                list.add(model)
+                            }
+                            adapter = MediaAdapter(list, requireActivity())
+                            mediaRecyclerView.adapter = adapter
+                            /*if (list.size == 0) {
+                                tempMediaText.visibility = View.VISIBLE
+                            } else {
+                                tempMediaText.visibility = View.GONE
+                            }*/
+                        }
+                    }
+
+                    Constants.MEDIA_TYPE_WHATSAPP_BUSINESS_VIDEOS -> {
+                        viewModel.whatsAppBusinessVideosLiveData.observe(requireActivity()) { unFilteredList ->
+                            val filteredList = unFilteredList.distinctBy { model ->
+                                model.fileName
+                            }
+
+                            val list = ArrayList<MediaModel>()
+                            filteredList.forEach { model ->
+                                list.add(model)
+                            }
+                            adapter = MediaAdapter(list, requireActivity())
+                            mediaRecyclerView.adapter = adapter
+                         /*   if (list.size == 0) {
+                                tempMediaText.visibility = View.VISIBLE
+                            } else {
+                                tempMediaText.visibility = View.GONE
+                            }*/
+                        }
+                    }
+                }
+
+
+
             }
         }
     }
