340cd17b29ec7305014a78ac6d4acab7bf57d4bb
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-12-02 17:40:57 +0500

    add two funcation create download dir & Get File Extension

--- app/src/main/java/com/muhsantech/statussaver/utils/FileUtils.kt
+++ app/src/main/java/com/muhsantech/statussaver/utils/FileUtils.kt
@@ -5,8 +5,18 @@ import android.os.Environment
 import com.muhsantech.statussaver.R
 import java.io.File
 
-fun Context.isStatusExist(fileName:String): Boolean {
+fun Context.isStatusExist(fileName: String): Boolean {
     val downloadDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
-    val file = File("${downloadDir}/${getString(R.string.app_name)}",)
+    val file = File("${downloadDir}/${getString(R.string.app_name)}", fileName)
     return file.exists()
+}
+
+
+fun getFileExtension(fileName: String): String {
+    val lastDotIndex = fileName.lastIndexOf(".")
+
+    if (lastDotIndex >= 0 && lastDotIndex < fileName.length - 1) {
+        return fileName.substring(lastDotIndex + 1)
+    }
+    return ""
 }
