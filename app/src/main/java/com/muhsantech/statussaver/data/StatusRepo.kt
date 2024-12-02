20b555b89eacf3fded23bbc9c136d1211a8a96ff
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-12-02 17:42:40 +0500

    Repo Class

--- app/src/main/java/com/muhsantech/statussaver/data/StatusRepo.kt
+++ app/src/main/java/com/muhsantech/statussaver/data/StatusRepo.kt
@@ -3,51 +3,101 @@ package com.muhsantech.statussaver.data
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.core.net.toUri
 import androidx.documentfile.provider.DocumentFile
 import androidx.lifecycle.MutableLiveData
 import com.anggrayudi.storage.FileWrapper
+import com.muhsantech.statussaver.models.MEDIA_TYPE_IMAGE
+import com.muhsantech.statussaver.models.MEDIA_TYPE_VIDEO
 import com.muhsantech.statussaver.models.MediaModel
 import com.muhsantech.statussaver.utils.Constants
 import com.muhsantech.statussaver.utils.SharedPrefKeys
 import com.muhsantech.statussaver.utils.SharedPrefUtils
+import com.muhsantech.statussaver.utils.getFileExtension
+import com.muhsantech.statussaver.utils.isStatusExist
 
 class StatusRepo(val context: Context) {
     val whatsAppStatusesLiveData = MutableLiveData<ArrayList<MediaModel>>()
     val whatsAppBusinessStatusesLiveData = MutableLiveData<ArrayList<MediaModel>>()
 
-    val activity  = context as Activity
+    val activity = context as Activity
 
     private val wpStatusesList = ArrayList<MediaModel>()
     private val wpBusinessStatusesList = ArrayList<MediaModel>()
 
-    fun getAllStatusesUser(whatsAppType: String = Constants.TYPE_WHATSAPP_MAIN){
+    private val TAG = "StatusRepo"
 
-        val treeUri = when(whatsAppType){
-            Constants.TYPE_WHATSAPP_MAIN->{
+    fun getAllStatuses(whatsAppType: String = Constants.TYPE_WHATSAPP_MAIN) {
+        val treeUri = when (whatsAppType) {
+            Constants.TYPE_WHATSAPP_MAIN -> {
                 SharedPrefUtils.getPrefString(SharedPrefKeys.PREF_KEY_WP_TREE_URI, "")?.toUri()!!
             }
-            else->{
-                SharedPrefUtils.getPrefString(SharedPrefKeys.PREF_KEY_WP_BUSINESS_TREE_URI, "")?.toUri()!!
 
+            else -> {
+                SharedPrefUtils.getPrefString(SharedPrefKeys.PREF_KEY_WP_BUSINESS_TREE_URI, "")
+                    ?.toUri()!!
             }
         }
+        Log.d(TAG, "getAllStatuses: $treeUri")
 
-        activity.contentResolver.takePersistableUriPermission(treeUri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        activity.contentResolver.takePersistableUriPermission(
+            treeUri,
+            Intent.FLAG_GRANT_READ_URI_PERMISSION
+        )
 
         val fileDocument = DocumentFile.fromTreeUri(activity, treeUri)
 
         fileDocument?.let {
             it.listFiles().forEach { file->
+                Log.d(TAG, "getAllStatuses: ${file.name}")
+                if (file.name != ".nomedia" && file.isFile) {
+                    val isDownloaded = context.isStatusExist(file.name!!)
+                    Log.d(TAG, "getAllStatusesExtension: Extension: ${getFileExtension(file.name!!)} ||${file.name}")
+                    val type = if (getFileExtension(file.name!!) == "mp4") {
+                        MEDIA_TYPE_VIDEO
+                    } else {
+                        MEDIA_TYPE_IMAGE
+                    }
 
-                if (file.name != ".nomedia" && file.isFile){
+                    val model = MediaModel(
+                        pathUri = file.uri.toString(),
+                        fileName = file.name!!,
+                        type = type,
+                        isDownloaded = isDownloaded
+                    )
+                    when (whatsAppType) {
+                        Constants.TYPE_WHATSAPP_MAIN -> {
 
+                            wpStatusesList.add(model)
+                        }
+
+                        else -> {
+                            wpBusinessStatusesList.add(model)
+                        }
+
+                    }
+
+                }
+            }
+            when (whatsAppType) {
+                Constants.TYPE_WHATSAPP_MAIN -> {
+                    Log.d(TAG, "getAllStatuses: Pushing Value to Wp live Data")
+                    whatsAppStatusesLiveData.postValue(wpStatusesList)
+                }
+
+                else -> {
+                    Log.d(TAG, "getAllStatuses: Pushing Value to Wp Business live Data")
+                    whatsAppBusinessStatusesLiveData.postValue(wpBusinessStatusesList)
                 }
 
             }
+
         }
 
+
     }
+
 
 
 }
