c1713e48e537131754b4d26d4f1fb39c1f60416a
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-12-02 17:42:00 +0500

    override create function and return repo

--- app/src/main/java/com/muhsantech/statussaver/viewmodels/factories/StatusViewModelFactory.kt
+++ app/src/main/java/com/muhsantech/statussaver/viewmodels/factories/StatusViewModelFactory.kt
@@ -1,4 +1,13 @@
 package com.muhsantech.statussaver.viewmodels.factories
 
-class StatusViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.muhsantech.statussaver.data.StatusRepo
+import com.muhsantech.statussaver.viewmodels.StatusViewModel
+
+class StatusViewModelFactory(private val repo: StatusRepo): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return StatusViewModel(repo) as T
+    }
 }
