589bb84a42c51362e7a15de20c20911a1044839e
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-12-03 10:49:24 +0500

    videos downloaded code

--- app/src/main/java/com/muhsantech/statussaver/utils/FileUtils.kt
+++ app/src/main/java/com/muhsantech/statussaver/utils/FileUtils.kt
@@ -1,8 +1,12 @@
 package com.muhsantech.statussaver.utils
 
+import android.content.ContentValues
 import android.content.Context
 import android.os.Environment
+import android.provider.MediaStore
+import androidx.core.net.toUri
 import com.muhsantech.statussaver.R
+import com.muhsantech.statussaver.models.MediaModel
 import java.io.File
 
 fun Context.isStatusExist(fileName: String): Boolean {
@@ -19,4 +23,40 @@ fun getFileExtension(fileName: String): String {
         return fileName.substring(lastDotIndex + 1)
     }
     return ""
+}
+
+fun Context.saveStatus(model: MediaModel): Boolean {
+    if (isStatusExist(model.fileName)){
+        return true
+    }
+    val extension = getFileExtension(model.fileName)
+    val mimeType = "${model.type}/$extension"
+    val inputStream = contentResolver.openInputStream(model.pathUri.toUri())
+    try {
+        val values = ContentValues()
+        values.apply {
+            put(MediaStore.MediaColumns.MIME_TYPE, mimeType)
+            put(MediaStore.MediaColumns.DISPLAY_NAME, model.fileName)
+            put(
+                MediaStore.MediaColumns.RELATIVE_PATH,
+                Environment.DIRECTORY_DOCUMENTS + "/" + getString(R.string.app_name)
+            )
+        }
+        val uri = contentResolver.insert(
+            MediaStore.Files.getContentUri("external"),
+            values
+        )
+        uri?.let {
+            val outputStream = contentResolver.openOutputStream(it)
+            if (inputStream != null) {
+                outputStream?.write(inputStream.readBytes())
+            }
+            outputStream?.close()
+            inputStream?.close()
+            return true
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    return false
 }
