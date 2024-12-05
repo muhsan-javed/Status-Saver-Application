940da3ea1b0304c334ff464a15e3712baf62af47
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-12-05 09:42:08 +0500

    Refresh UI functional and fixing

--- app/src/main/java/com/muhsantech/statussaver/view/fragments/FragmentStatus.kt
+++ app/src/main/java/com/muhsantech/statussaver/view/fragments/FragmentStatus.kt
@@ -2,10 +2,14 @@ package com.muhsantech.statussaver.view.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.tabs.TabLayout
@@ -39,72 +43,85 @@ class FragmentStatus : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding.apply {
+        binding.apply {
             arguments?.let {
-
                 val repo = StatusRepo(requireActivity())
                 viewModel = ViewModelProvider(
                     requireActivity(),
                     StatusViewModelFactory(repo)
                 )[StatusViewModel::class.java]
 
+                type = it.getString(Constants.FRAGMENT_TYPE_KEY, "")
 
-                type = it.getString(Constants.FRAGMENT_TYPE_KEY,"")
-                tempText.text = type
-                when(type){
-                    Constants.TYPE_WHATSAPP_MAIN->{
-                        //WhatsApp Status
-                        // Check permission
+                when (type) {
+                    Constants.TYPE_WHATSAPP_MAIN -> {
+                        // check permission
                         // granted then fetch statuses
-                        // get Permission
-                        //fetch statuses
-                        val  isPermissionGranted = SharedPrefUtils.getPrefBoolean(SharedPrefKeys.PREF_KEY_WP_PERMISSION_GRANTED, false)
-                        if (isPermissionGranted){
-                            getWhatsappStatuses()
+                        // get permission
+                        // fetch statuses
+                        val isPermissionGranted = SharedPrefUtils.getPrefBoolean(
+                            SharedPrefKeys.PREF_KEY_WP_PERMISSION_GRANTED,
+                            false
+                        )
+                        if (isPermissionGranted) {
+                            getWhatsAppStatuses()
+
+                            binding.swipeRefreshLayout.setOnRefreshListener {
+                                refreshStatuses()
+                            }
+
                         }
                         permissionLayout.btnPermissions.setOnClickListener {
                             getFolderPermissions(
-                                context = requireContext(),
+                                context = requireActivity(),
                                 REQUEST_CODE = WHATSAPP_REQUEST_CODE,
                                 initialUri = Constants.getWhatsappUri()
                             )
                         }
 
+
                         val viewPagerAdapter = MediaViewPagerAdapter(requireActivity())
                         statusViewPager.adapter = viewPagerAdapter
-                        TabLayoutMediator(tabLayout, statusViewPager) { tab, pos->
+                        TabLayoutMediator(tabLayout, statusViewPager) { tab, pos ->
                             tab.text = viewPagerTitles[pos]
                         }.attach()
 
                     }
-                    Constants.TYPE_WHATSAPP_BUSINESS-> {
-                        val  isPermissionGranted = SharedPrefUtils.getPrefBoolean(SharedPrefKeys.PREF_KEY_WP_BUSINESS_PERMISSION_GRANTED, false)
-                        if (isPermissionGranted){
-                            getWhatsappBusinessStatuses()
+
+                    Constants.TYPE_WHATSAPP_BUSINESS -> {
+                        val isPermissionGranted = SharedPrefUtils.getPrefBoolean(
+                            SharedPrefKeys.PREF_KEY_WP_BUSINESS_PERMISSION_GRANTED,
+                            false
+                        )
+                        if (isPermissionGranted) {
+                            getWhatsAppBusinessStatuses()
+
+                            binding.swipeRefreshLayout.setOnRefreshListener {
+                                refreshStatuses()
+                            }
+
                         }
-                        //WhatsApp Business Status
                         permissionLayout.btnPermissions.setOnClickListener {
                             getFolderPermissions(
-                                context = requireContext(),
+                                context = requireActivity(),
                                 REQUEST_CODE = WHATSAPP_BUSINESS_REQUEST_CODE,
                                 initialUri = Constants.getWhatsappBusinessUri()
                             )
                         }
-
-
-                        val viewPagerAdapter = MediaViewPagerAdapter(requireActivity(),
-                            Constants.MEDIA_TYPE_WHATSAPP_BUSINESS_IMAGES,Constants.MEDIA_TYPE_WHATSAPP_BUSINESS_VIDEOS)
+                        val viewPagerAdapter = MediaViewPagerAdapter(
+                            requireActivity(),
+                            imagesType = Constants.MEDIA_TYPE_WHATSAPP_BUSINESS_IMAGES,
+                            videosType = Constants.MEDIA_TYPE_WHATSAPP_BUSINESS_VIDEOS
+                        )
                         statusViewPager.adapter = viewPagerAdapter
-                        TabLayoutMediator(tabLayout, statusViewPager) { tab, pos->
+                        TabLayoutMediator(tabLayout, statusViewPager) { tab, pos ->
                             tab.text = viewPagerTitles[pos]
                         }.attach()
                     }
                 }
 
-           }
-
-       }
-
+            }
+        }
     }
 
     override fun onCreateView(
@@ -113,48 +130,72 @@ class FragmentStatus : Fragment() {
     ) = binding.root
 
     // funcation to get whatsapp statuses
-    fun getWhatsappStatuses(){
+    fun getWhatsAppStatuses() {
+        // function to get wp statuses
         binding.permissionLayoutHolder.visibility = View.GONE
         viewModel.getWhatsAppStatuses()
     }
 
-    fun getWhatsappBusinessStatuses(){
+    private val TAG = "FragmentStatus"
+    fun getWhatsAppBusinessStatuses() {
+        // function to get wp statuses
         binding.permissionLayoutHolder.visibility = View.GONE
+        Log.d(TAG, "getWhatsAppBusinessStatuses: Getting Wp Business Statuses")
         viewModel.getWhatsAppBusinessStatuses()
+    }
+
+    fun refreshStatuses() {
+        when (type) {
+            Constants.TYPE_WHATSAPP_MAIN -> {
+                Toast.makeText(requireActivity(), "Refreshing WP Statuses", Toast.LENGTH_SHORT)
+                    .show()
+                getWhatsAppStatuses()
+            }
+
+            else -> {
+                Toast.makeText(
+                    requireActivity(),
+                    "Refreshing WP Business Statuses",
+                    Toast.LENGTH_SHORT
+                ).show()
+                getWhatsAppBusinessStatuses()
+            }
+        }
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            binding.swipeRefreshLayout.isRefreshing = false
+        }, 2000)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-
-        if (resultCode == AppCompatActivity.RESULT_OK){
+        if (resultCode == AppCompatActivity.RESULT_OK) {
             val treeUri = data?.data!!
             requireActivity().contentResolver.takePersistableUriPermission(
-                treeUri,Intent.FLAG_GRANT_READ_URI_PERMISSION
+                treeUri,
+                Intent.FLAG_GRANT_READ_URI_PERMISSION
             )
-
-            if (requestCode == WHATSAPP_REQUEST_CODE){
+            if (requestCode == WHATSAPP_REQUEST_CODE) {
                 // whatsapp logic here
                 SharedPrefUtils.putPrefString(
                     SharedPrefKeys.PREF_KEY_WP_TREE_URI,
                     treeUri.toString()
                 )
-
                 SharedPrefUtils.putPrefBoolean(SharedPrefKeys.PREF_KEY_WP_PERMISSION_GRANTED, true)
-                getWhatsappStatuses()
-            }
-            else if (requestCode == WHATSAPP_BUSINESS_REQUEST_CODE){
-                //Business whatsapp logic here
+                getWhatsAppStatuses()
+            } else if (requestCode == WHATSAPP_BUSINESS_REQUEST_CODE) {
+                // whatsapp business logic here
                 SharedPrefUtils.putPrefString(
                     SharedPrefKeys.PREF_KEY_WP_BUSINESS_TREE_URI,
                     treeUri.toString()
                 )
-
-                SharedPrefUtils.putPrefBoolean(SharedPrefKeys.PREF_KEY_WP_BUSINESS_PERMISSION_GRANTED, true)
-                getWhatsappBusinessStatuses()
+                SharedPrefUtils.putPrefBoolean(
+                    SharedPrefKeys.PREF_KEY_WP_BUSINESS_PERMISSION_GRANTED,
+                    true
+                )
+                getWhatsAppBusinessStatuses()
             }
-
-
         }
 
 
