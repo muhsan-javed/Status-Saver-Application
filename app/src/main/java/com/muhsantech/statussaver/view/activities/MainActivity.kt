91783c4a80c525cb2136e066607cb01f49c22a3f
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2024-12-05 09:39:34 +0500

    android 9 code get permission And splash screen setup

--- app/src/main/java/com/muhsantech/statussaver/view/activities/MainActivity.kt
+++ app/src/main/java/com/muhsantech/statussaver/view/activities/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.muhsantech.statussaver.view.activities
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.muhsantech.statussaver.R
 import com.muhsantech.statussaver.databinding.ActivityMainBinding
 import com.muhsantech.statussaver.utils.Constants
+import com.muhsantech.statussaver.utils.SharedPrefKeys
 import com.muhsantech.statussaver.utils.SharedPrefUtils
 import com.muhsantech.statussaver.utils.replaceFragment
+import com.muhsantech.statussaver.utils.slideFromStart
+import com.muhsantech.statussaver.utils.slideToEndWithFadeOut
 import com.muhsantech.statussaver.view.fragments.FragmentSettings
 import com.muhsantech.statussaver.view.fragments.FragmentStatus
 
@@ -24,45 +35,102 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         SharedPrefUtils.init(activity)
 
-        //Default whatsapp status
-        val fragmentWhatsAppStatus = FragmentStatus()
-        val bundle = Bundle()
-        bundle.putString(Constants.FRAGMENT_TYPE_KEY, Constants.TYPE_WHATSAPP_MAIN)
-        replaceFragment(fragmentWhatsAppStatus, bundle)
-
         binding.apply {
+            splashLogic()
+            requestPermission()
+            val fragmentWhatsAppStatus = FragmentStatus()
+            val bundle = Bundle()
+            bundle.putString(Constants.FRAGMENT_TYPE_KEY, Constants.TYPE_WHATSAPP_MAIN)
+            replaceFragment(fragmentWhatsAppStatus, bundle)
+            toolBar.setNavigationOnClickListener {
+                drawerLayout.open()
+            }
 
-            bottomNavigationView.setOnItemSelectedListener {
-                when(it.itemId){
-                    R.id.menu_status->{
-                        //whatspp status
+
+            navigationView.setNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.menu_status -> {
+                        // whatsapp status
                         val fragmentWhatsAppStatus = FragmentStatus()
                         val bundle = Bundle()
                         bundle.putString(Constants.FRAGMENT_TYPE_KEY, Constants.TYPE_WHATSAPP_MAIN)
                         replaceFragment(fragmentWhatsAppStatus, bundle)
+                        drawerLayout.close()
                     }
-                    R.id.menu_business_status->{
-                        //whatspp Buusines status
+
+                    R.id.menu_business_status -> {
+                        // whatsapp business status
                         val fragmentWhatsAppStatus = FragmentStatus()
                         val bundle = Bundle()
-                        bundle.putString(Constants.FRAGMENT_TYPE_KEY, Constants.TYPE_WHATSAPP_BUSINESS)
+                        bundle.putString(
+                            Constants.FRAGMENT_TYPE_KEY,
+                            Constants.TYPE_WHATSAPP_BUSINESS
+                        )
                         replaceFragment(fragmentWhatsAppStatus, bundle)
+                        drawerLayout.close()
                     }
-                    R.id.menu_settings->{
+
+                    R.id.menu_settings -> {
+                        // settings
                         replaceFragment(FragmentSettings())
+                        drawerLayout.close()
                     }
                 }
-                return@setOnItemSelectedListener true
+
+                return@setNavigationItemSelectedListener true
             }
 
         }
 
-      /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
+    }
 
+
+    private val PERMISSION_REQUEST_CODE = 50
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
+            val isPermissionsGranted = SharedPrefUtils.getPrefBoolean(
+                SharedPrefKeys.PREF_KEY_IS_PERMISSIONS_GRANTED,
+                false
+            )
+            if (!isPermissionsGranted) {
+                ActivityCompat.requestPermissions(
+                    /* activity = */ activity,
+                    /* permissions = */ arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    /* requestCode = */ PERMISSION_REQUEST_CODE
+                )
+                Toast.makeText(activity, "Please Grant Permissions", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            val isGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+            if (isGranted) {
+                SharedPrefUtils.putPrefBoolean(SharedPrefKeys.PREF_KEY_IS_PERMISSIONS_GRANTED, true)
+            } else {
+                SharedPrefUtils.putPrefBoolean(
+                    SharedPrefKeys.PREF_KEY_IS_PERMISSIONS_GRANTED,
+                    false
+                )
+
+            }
+        }
+    }
+
+    private fun splashLogic() {
+        binding.apply {
+            splashLayout.cardView.slideFromStart()
+            Handler(Looper.myLooper()!!).postDelayed({
+                splashScreenHolder.slideToEndWithFadeOut()
+                splashScreenHolder.visibility = View.GONE
+            }, 3000)
+        }
     }
 
 
